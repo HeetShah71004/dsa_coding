3afa57bb905395cc93c76335a7f557bd0ccc3184
Brad Traversy <me@bradTraversy.com>
2025-04-01 09:26:41 +0530

    Dsa_coding

---
+++ Dailycode/solving-questions-with-brainpower.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def rec(self, i: int, questions: List[List[int]], dp: List[int], n: int) -> int:
+        if i >= n:
+            return 0
+        if dp[i] != -1:
+            return dp[i]
+
+        take = questions[i][0] + self.rec(i + questions[i][1] + 1, questions, dp, n)
+        dont = self.rec(i + 1, questions, dp, n)
+
+        dp[i] = max(take, dont)
+        return dp[i]
+
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        n = len(questions)
+        dp = [-1] * n
+        return self.rec(0, questions, dp, n)
+
+# Test cases
+
+questions = [[3,2],[4,3],[4,4],[2,5]]
+
+print(Solution().mostPoints(questions)) # Output: 5
