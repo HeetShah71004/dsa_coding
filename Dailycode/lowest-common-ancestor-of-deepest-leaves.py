2dc994a9a7f94e50fc79937b1ec5e6682f208223
Brad Traversy <me@bradTraversy.com>
2025-04-04 10:53:27 +0530

    Dsa_coding

--- Dailycode/lowest-common-ancestor-of-deepest-leaves.py
+++ Dailycode/lowest-common-ancestor-of-deepest-leaves.py
@@ -64,14 +64,6 @@ def build_tree(values):
     return root
 
 # --- Test Case ---
-# Tree structure:
-#         1
-#       /   \
-#      2     3
-#     /     / \
-#    4     5   6
-#             /
-#            7
 
 values = [3,5,1,6,2,0,8,None,None,7,4]
 root = build_tree(values)
